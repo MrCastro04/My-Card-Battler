0d2f42365cd5630e9c40215a6046fe33dd2dbc10
MrCastro04 <slavamora684@gmail.com>
2025-06-25 17:52:43 +0300

    Card View э

---
+++ Card Battler/Assets/Modules/Content/Card/Scripts/CardData.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Modules.Content.Card.Scripts
+{
+    [CreateAssetMenu(menuName = "Data/Card")]
+    public class CardData : ScriptableObject
+    {
+        [field: SerializeField] public int ManaAmount { get; private set; }
+        [field: SerializeField] public string Name { get; private set; }
+        [field: SerializeField] public Sprite Image { get; private set; }
+    }
+}
---
+++ Card Battler/Assets/Modules/Content/Card/Scripts/CardView.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Modules.Content.Card.Scripts
+{
+    public class CardView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _mana;
+        [SerializeField] private TMP_Text _name;
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private GameObject _wrapper;
+
+        public void Setup(CardModel cardModel)
+        {
+            _mana.text = cardModel.ManaAmount.ToString();
+
+            _name.text = cardModel.Name;
+
+            _spriteRenderer.sprite = cardModel.Image;
+        }
+    }
+}
