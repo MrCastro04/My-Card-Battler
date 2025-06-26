4323abdc2c032b7153526f7b8e37dcd53f7dcde3
MrCastro04 <slavamora684@gmail.com>
2025-06-26 12:40:05 +0300

    Обновил фабриику

--- Card Battler/Assets/Modules/Core/Factories/FactoriesInstaller.cs
+++ Card Battler/Assets/Modules/Core/Factories/FactoriesInstaller.cs
@@ -1,17 +1,13 @@
-﻿using UnityEngine;
-using Zenject;
+﻿using Zenject;
 
 namespace Modules.Core.Factories
 {
     public class FactoriesInstaller : MonoInstaller
     {
-        [SerializeField] private CardViewFactory _cardViewFactory;
-        
         public override void InstallBindings()
         {
             Container
                 .BindInterfacesTo<CardViewFactory>()
-                .FromInstance(_cardViewFactory)
                 .AsSingle()
                 .NonLazy();
         }
