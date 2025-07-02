3ba78197e79bc4c11b570adcc441d69cd3604315
MrCastro04 <slavamora684@gmail.com>
2025-07-02 12:53:55 +0300

    реф

--- Card Battler/Assets/Modules/Core/Zenject/Systems Installers/Discard Pile System Installer/DiscardPileSystemInstaller.cs
+++ Card Battler/Assets/Modules/Core/Zenject/Systems Installers/Discard Pile System Installer/DiscardPileSystemInstaller.cs
@@ -1,19 +1,26 @@
-﻿using Modules.Core.Systems.Discard_Pile_System;
+﻿using Modules.Core.Systems.Deck_System;
+using Modules.Core.Systems.Discard_Pile_System;
 using UnityEngine;
 using Zenject;
 
-namespace Modules.New
+namespace Modules.Core.Zenject.Systems_Installers.Discard_Pile_System_Installer
 {
     public class DiscardPileSystemInstaller : MonoInstaller
     {
-        [SerializeField] private Vector3 _discardPilePosition;
+        [SerializeField] private float _downOffsetFromDeck;
         
         public override void InstallBindings()
         {
+            DeckSystem deckSystem = Container.Resolve<DeckSystem>();
+
+            Vector3 deckPosition = deckSystem.Position;
+            
+            Vector3 discardPilePosition = new Vector3(deckPosition.x,deckPosition.y + _downOffsetFromDeck, deckPosition.z );
+            
             Container
                 .BindInterfacesAndSelfTo<DiscardPileSystem>()
                 .AsSingle()
-                .WithArguments(_discardPilePosition)
+                .WithArguments(discardPilePosition)
                 .NonLazy();
         }
     }
